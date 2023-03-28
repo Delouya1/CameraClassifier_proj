8dd781f6d892c02925ecc4b6993265ce24f655d7
Delouya1 <delouya1@gmail.com>
2023-03-28 13:00:16 +0300

    Camera class: Ctor, Dtor and a method to capture the next frame.

---
+++ camera.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+
+class Camera:
+    def __init__(self):  # constructor
+        self.camera = cv.VideoCapture(0)
+        if not self.camera.isOpened():
+            raise Exception("Camera not found")
+
+    def __del__(self):  # destructor
+        if self.camera.isOpened():
+            self.camera.release()
+
+    def get_frame(self):  # get the current frame from the camera
+        if self.camera.isOpened():
+            ret, frame = self.camera.read()
+            if ret:  # if the frame was successfully read
+                return (ret, cv.cvtColor(frame, cv.COLOR_BGR2RGB))  # convert the image to RGB
+        return None
+
+
